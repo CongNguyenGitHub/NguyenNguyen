5dc85cbd3e0a0f6090a2bf961e374f3e7b9c333d
CongNguyenGitHub <114802813+CongNguyenGitHub@users.noreply.github.com>
2022-10-01 16:50:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-st.session_state['answer'] = ''!
-st.write(st.session_state)
-realans = ['', 'abc', 'edf']
-if  st.session_state['answer'] in realans:
-    answerStat = "correct"
-elif st.session_state['answer'] not in realans:
-    answerStat = "incorrect"
-st.write(st.session_state)
-st.write(answerStat)
 st.set_page_config(page_title="CAY NHA LA VUON")
 data_file=st.file_uploader("Input your file here ",type="xlsx")
 if data_file is not None :
