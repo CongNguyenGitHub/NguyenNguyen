e06cacf4c0d165df84586129577a3976b2a37852
CongNguyenGitHub <114802813+CongNguyenGitHub@users.noreply.github.com>
2022-10-04 19:21:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ if data_file is not None :
             b=tf.Variable(np.random.randn(),dtype=tf.float32)
             pred=tf.add(tf.matmul(X,W),b)
             loss=tf.compat.v1.reduce_mean (tf.square(pred-Y))
-            optimizer=tf.compat.v1.train.GradientDescentOptimizer(learning_rate= 0.01).minimize(loss)
+            optimizer=tf.compat.v1.train.GradientDescentOptimizer(learning_rate= 0.1).minimize(loss)
             init=tf.compat.v1.global_variables_initializer ()
             sess=tf.compat.v1.Session()
             sess.run(init)
